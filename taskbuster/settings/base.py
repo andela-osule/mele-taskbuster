0979faf80ed70c568c51e6db42d1277a2a97b2a7
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-11-03 16:15:52 +0100

    Internationalization and localization

--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from django.core.exceptions import ImproperlyConfigured
+from django.utils.translation import ugettext_lazy as _
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -47,6 +48,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -55,6 +57,17 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 )
+
+
+LANGUAGES = (
+    ('en', _('English')),
+    ('ca', _('Catalan')),
+)
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
+
 
 ROOT_URLCONF = 'taskbuster.urls'
 
@@ -70,6 +83,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
             ],
         },
     },
@@ -95,7 +109,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
--- taskbuster/test.py
+++ taskbuster/test.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
+from django.utils.translation import activate
 from django.core.urlresolvers import reverse
 
 
 class TestHomePage(TestCase):
+
+    def setUp(self):
+        activate('en')
 
     def test_uses_index_template(self):
         response = self.client.get(reverse("home"))
@@ -12,3 +16,11 @@ class TestHomePage(TestCase):
     def test_uses_base_template(self):
         response = self.client.get(reverse("home"))
         self.assertTemplateUsed(response, "base.html")
+
+    def test_internationalization(self):
+        for lang, h1_text in [('en', 'Welcome to TaskBuster!'),
+                              ('ca', 'Benvingut a TaskBuster!')]:
+            activate(lang)
+            self.browser.get(self.get_full_url("home"))
+            h1 = self.browser.find_element_by_tag_name("h1")
+            self.assertEqual(h1.text, h1_text)
